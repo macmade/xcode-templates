307c18e14712e42627fb155095755ce492f9d7b5
macmade <macmade@xs-labs.com>
2019-09-12 22:31:38 +0200

    Templates update...

--- XS/Swift File.xctemplate/Proprietary/___FILEBASENAME___.swift
+++ XS/Swift File.xctemplate/Proprietary/___FILEBASENAME___.swift
@@ -1,6 +1,10 @@
 /*******************************************************************************
  * Copyright (c) ___YEAR___, Jean-David Gadina - www.xs-labs.com
  * All rights reserved
+ * 
+ * Unauthorised copying of this copyrighted work, via any medium is strictly
+ * prohibited.
+ * Proprietary and confidential.
  ******************************************************************************/
 
 
