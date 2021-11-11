c440a467676dd24cee96c47b02eb0c74fe58e8a9
macmade <macmade@xs-labs.com>
2021-11-11 12:27:04 +0100

    Updated Xcode templates.

---
+++ XS/Objective-C Class.xctemplate/GPLHeader/___FILEBASENAME___.m
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) ___YEAR___, Jean-David Gadina - www.xs-labs.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+/*!
+ * @file        ___FILENAME___
+ * @copyright   (c) ___YEAR___, Jean-David Gadina - www.xs-labs.com
+ */
+
+#import "___FILEBASENAME___.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ___FILEBASENAMEASIDENTIFIER___()
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+@end
--- XS/Objective-C Class.xctemplate/MITNo/___FILEBASENAME___.h
+++ XS/Objective-C Class.xctemplate/MITNo/___FILEBASENAME___.h
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * The MIT License (MIT)
  * 
- * Copyright (c) ___YEAR___ Jean-David Gadina - www.xs-labs.com
+ * Copyright (c) ___YEAR___, Jean-David Gadina - www.xs-labs.com
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
